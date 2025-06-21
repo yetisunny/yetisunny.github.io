585a06bb6dd54aada7a33d6b41faf41e94149e27
Luuk van der Heijden <luukjamal1404@hotmail.com>
2025-06-21 13:00:14 +0200

    Quartz sync: Jun 21, 2025, 1:00 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -34,7 +34,6 @@ export const defaultContentPageLayout: PageLayout = {
           Component: Component.Search(),
           grow: true,
         },
-        { Component: Component.Darkmode() },
         { Component: Component.ReaderMode() },
       ],
     }),
