72e603175709f544c84b7393c759aa562de565be
Luuk van der Heijden <luukjamal1404@hotmail.com>
2025-06-24 12:00:44 +0200

    Quartz sync: Jun 24, 2025, 12:00 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -44,7 +44,7 @@ const config: QuartzConfig = {
           lightgray: "#313244",    // surface0
           gray: "#45475a",         // surface1
           darkgray: "#6c7086",     // surface2
-          dark: "#a6e3a1",         // green
+          dark: "#a9e3a1",         // green
           secondary: "#a6e3a1",    // green
           tertiary: "#94e2d5",     // teal
           highlight: "rgba(166, 227, 161, 0.15)", // green with alpha
